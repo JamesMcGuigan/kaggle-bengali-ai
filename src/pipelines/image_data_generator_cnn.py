d6382b24c0a88508562e3a394f15ddd915225b02
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-03-18 22:14:03 +0000

    csv | optimise submission.csv for Kaggle

--- src/pipelines/image_data_generator_cnn.py
+++ src/pipelines/image_data_generator_cnn.py
@@ -12,9 +12,11 @@ from src.dataset.Transforms import Transforms
 from src.models.MultiOutputCNN import MultiOutputCNN
 from src.settings import settings
 from src.util.argparse import argparse_from_dicts
-from src.util.csv import df_to_submission_csv, submission_df
-from src.util.hparam import hparam_key, model_compile, model_stats_from_history, callbacks
+from src.util.csv import df_to_submission_csv, submission_df_generator
+from src.util.hparam import callbacks, hparam_key, model_compile, model_stats_from_history
 from src.util.logs import log_model_stats
+
+
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 0, 1, 2, 3 # Disable Tensortflow Logging
 
@@ -174,9 +176,9 @@ if __name__ == '__main__':
         "patience":      10,
         "epochs":        99,
     }
-    if os.environ.get('KAGGLE_KERNEL_RUN_TYPE') == 'Interactive':
+    if os.environ.get('KAGGLE_KERNEL_RUN_TYPE', 'Interactive') == 'Interactive':
         train_hparams['patience'] = 0
-        train_hparams['loops']    = 1
+        train_hparams['epochs']   = 0
     train_hparams = { **settings['hparam_defaults'], **train_hparams }
 
     argparse_from_dicts([train_hparams, model_hparams], inplace=True)
@@ -192,5 +194,5 @@ if __name__ == '__main__':
 
     log_model_stats(model_stats, logfilename, model_hparams, train_hparams)
 
-    submission = submission_df(model, output_shape)
+    submission = submission_df_generator(model, output_shape)
     df_to_submission_csv( submission, csv_filename )
--- src/util/csv.py
+++ src/util/csv.py
@@ -1,12 +1,15 @@
-import gc
 import os
 
+import gc
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
 
-### Predict Output Submssion
 from src.dataset.DatasetDF import DatasetDF
+from src.dataset.ParquetImageDataGenerator import ParquetImageDataGenerator
+from src.dataset.Transforms import Transforms
+from src.settings import settings
+
 
 
 ### BUGFIX: Repeatedly calling model.predict(...) results in memory leak - https://github.com/keras-team/keras/issues/13118
@@ -18,55 +21,107 @@ def submission_df(model, output_shape):
     for data_id in range(0,4):
         test_dataset      = DatasetDF(test_train='test', data_id=data_id, transform_X_args = { "normalize": True } )
         test_dataset_rows = test_dataset.X['train'].shape[0]
-        batch_size        = 32
-        for index in range(0, test_dataset_rows, 32):
-            X_batch     = test_dataset.X['train'][index : index+batch_size]
-            predictions = model.predict_on_batch(X_batch)
-            # noinspection PyTypeChecker
-            submission = submission.append(
-                pd.DataFrame({
-                    key: np.argmax( predictions[index], axis=-1 )
-                    for index, key in enumerate(output_shape.keys())
-                }, index=test_dataset.ID['train'])
-            )
-    return submission
+        batch_size        = 64
+        for index in range(0, test_dataset_rows, batch_size):
+            try:
+                X_batch     = test_dataset.X['train'][index : index+batch_size]
+                predictions = model.predict_on_batch(X_batch)
+                # noinspection PyTypeChecker
+                submission = submission.append(
+                    pd.DataFrame({
+                        key: np.argmax( predictions[index], axis=-1 )
+                        for index, key in enumerate(output_shape.keys())
+                        }, index=test_dataset.ID['train'])
+                    )
+            except Exception as exception:
+                print('submission_df_generator()', exception)
+
+        return submission
 
 ###
 ### Use submission_df() it seems to have more success on Kaggle
 ###
-# def submission_df_generator(model, output_shape):
-#     gc.collect(); sleep(5)
-#
-#     # large datasets on submit, so loop via generator to avoid Out-Of-Memory errors
-#     submission = pd.DataFrame(columns=output_shape.keys())
-#     for batch in ParquetImageDataGenerator.batch_generator(
-#         f"{settings['dir']['data']}/test_image_data_*.parquet",
-#         reads_per_file = 3,
-#         resamples      = 1,
-#         shuffle        = False,
-#         infinite       = False,
-#     ):
-#         X = Transforms.transform_X(batch, normalize=True)
-#         predictions = model.predict_on_batch(X)
-#         submission = submission.append(
-#             pd.DataFrame({
-#                 key: np.argmax( predictions[index], axis=-1 )
-#                 for index, key in enumerate(output_shape.keys())
-#             }, index=batch['image_id'])
-#         )
+def submission_df_generator(model, output_shape):
+    gc.collect()
+
+    if os.environ.get('KAGGLE_KERNEL_RUN_TYPE', 'Interactive') == 'Interactive':
+        globpath = f"{settings['dir']['data']}/train_image_data_*.parquet"
+    else:
+        globpath = f"{settings['dir']['data']}/test_image_data_*.parquet"
+
+    # large datasets on submit, so loop via generator to avoid Out-Of-Memory errors
+    submission  = pd.DataFrame(columns=output_shape.keys())
+    cache_index = 0
+    for cache in ParquetImageDataGenerator.cache_generator(
+            globpath,
+            reads_per_file = 3,
+            resamples      = 1,
+            shuffle        = False,
+            infinite       = False,
+            ):
+        try:
+            cache_index      += 1
+            batch_size        = 64
+            test_dataset_rows = cache.shape[0]
+            print(f'submission_df_generator() - submission: ', cache_index, submission.shape)
+            if test_dataset_rows == 0: continue
+            for index in range(0, test_dataset_rows, batch_size):
+                try:
+                    batch = cache[index : index+batch_size]
+                    if batch.shape[0] == 0: continue
+                    X           = Transforms.transform_X(batch, normalize=True)
+                    predictions = model.predict_on_batch(X)
+                    submission  = submission.append(
+                        pd.DataFrame({
+                            key: np.argmax( predictions[index], axis=-1 )
+                            for index, key in enumerate(output_shape.keys())
+                            }, index=batch['image_id'])
+                        )
+                except Exception as exception:
+                    print('submission_df_generator() - batch', exception)
+        except Exception as exception:
+            print('submission_df_generator() - cache', exception)
+
+    return submission
+
+
+
+# def df_to_submission(df: DataFrame) -> DataFrame:
+#     print('df_to_submission() - input', df.shape)
+#     output_fields = ['consonant_diacritic', 'grapheme_root', 'vowel_diacritic']
+#     submission = DataFrame(columns=['row_id', 'target'])
+#     for index, row in df.iterrows():
+#         for output_field in output_fields:
+#             try:
+#                 index = f"Test_{index}" if not str(index).startswith('T') else index
+#                 submission = submission.append({
+#                     'row_id': f"{index}_{output_field}",
+#                     'target': df[output_field].loc[index],
+#                     }, ignore_index=True)
+#             except Exception as exception:
+#                 print('df_to_submission()', exception)
+#     print('df_to_submission() - output', submission.shape)
 #     return submission
 
 
 def df_to_submission(df: DataFrame) -> DataFrame:
+    print('df_to_submission_columns() - input', df.shape)
     output_fields = ['consonant_diacritic', 'grapheme_root', 'vowel_diacritic']
-    submission = DataFrame(columns=['row_id', 'target'])
-    for index, row in df.iterrows():
-        for output_field in output_fields:
-            index = f"Test_{index}" if not str(index).startswith('T') else index
-            submission = submission.append({
-                'row_id': f"{index}_{output_field}",
-                'target': df[output_field].loc[index],
-            }, ignore_index=True)
+    submissions = {}
+    for output_field in output_fields:
+        if 'image_id' in df.columns:
+            submissions[output_field] = DataFrame({
+                'row_id': df['image_id'] + '_' + output_field,
+                'target': df[output_field],
+            })
+        else:
+            submissions[output_field] = DataFrame({
+                'row_id': df.index + '_' + output_field,
+                'target': df[output_field],
+            })
+    submission = pd.concat(submissions.values())
+    submission = submission.sort_values(by='row_id')
+    print('df_to_submission_columns() - output', submission.shape)
     return submission
 
 
