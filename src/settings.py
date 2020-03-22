e76845000c43bfbf80668c4a97910ddcaf652a8e
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-03-22 15:31:04 +0000

    settings | os.makedirs | BUGFIX: read-only filesystem

--- src/settings.py
+++ src/settings.py
@@ -71,7 +71,9 @@ else:
 
 ####################
 if __name__ == '__main__':
-    for dirname in settings['dir'].values(): os.makedirs(dirname, exist_ok=True)
+    for dirname in settings['dir'].values():
+        try:    os.makedirs(dirname, exist_ok=True)  # BUGFIX: read-only filesystem
+        except: pass
     for key,value in settings.items():       print(f"settings['{key}']:".ljust(30), str(value))
 
     if os.environ.get('KAGGLE_KERNEL_RUN_TYPE'):
